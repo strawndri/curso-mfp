0577ea705d56e9699d60afa48e2beb3a5349fe6f
Andrieli <andrieliluci@gmail.com>
2024-05-15 07:28:03 -0300

    contests semana 2: E - Median of an Array

--- contests/semana-02/E_median_of_an_rray.cpp
+++ contests/semana-02/E_median_of_an_rray.cpp
@@ -4,6 +4,40 @@ using namespace std;
 
 int main() {
 
-    
+    long t, n, operacoes, central;
+    long long a;
+
+    cin >> t;
+
+    for (long i = 0; i < t; i++) {
+        cin >> n;
+        
+        vector<long long> sequencia;
+
+        for (long j = 0; j < n; j++) {
+            cin >> a;
+            sequencia.push_back(a);
+        }
+
+        sort(sequencia.begin(), sequencia.end());
+
+        if (n % 2 == 0) {
+            central = (n/2) - 1;
+        } else {
+            central = (n/2);
+        }
+
+        operacoes = 0;
+        for (long k = central; k < n; k++) {
+
+            if (sequencia[k] == sequencia[central]) {
+                operacoes++;
+            } else {
+                break;
+            }
+        }
+        
+        cout << operacoes << endl;
+    }
     return 0;
 }
