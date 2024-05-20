3e50828446004d89c74af194ebc8aab35c3c8afb
Andrieli <andrieliluci@gmail.com>
2024-05-20 10:11:46 -0300

    contests semana 3: B - Maximum Subarray Sum

---
+++ contests/semana-03/B_maximum_subarray_sum.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    long n;
+    cin >> n;
+
+    vector<long long> v(n), pref(n);
+
+    for (int i = 0; i < n; i++) {
+        cin >> v[i];
+    }
+
+    pref[0] = v[0];
+    long long max_subarray_sum = pref[0];
+
+    for (long i = 1; i < n; i++) {
+        pref[i] = max(v[i], v[i] + pref[i - 1]);
+        max_subarray_sum = max(max_subarray_sum, pref[i]);
+    }
+
+    cout << max_subarray_sum << endl;
+    
+    return 0;
+}
