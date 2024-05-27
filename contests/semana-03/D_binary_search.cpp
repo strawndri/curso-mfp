c0072115d78d751759fce5410e9b3cfe714aa088
Andrieli <andrieliluci@gmail.com>
2024-05-27 17:52:39 -0300

    contests semana 3: D

--- contests/semana-03/D_binary_search.cpp
+++ contests/semana-03/D_binary_search.cpp
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #include <bits/stdc++.h>
 
 using namespace std;
@@ -33,40 +32,4 @@ int main() {
     }
 
     return 0;
-=======
-#include <bits/stdc++.h>
-
-using namespace std;
-
-int main() {
-    ios::sync_with_stdio(false);
-    cin.tie(NULL);
-
-    long n, q;
-    cin >> n >> q;
-
-    map<long long, long long> posicoes;
-
-    for (long i = 0; i < n; i++) {
-        long long a;
-        cin >> a;
-        
-        if (posicoes.find(a) == posicoes.end()) {
-            posicoes[a] = i;
-        }
-    }
-
-    while (q--) {
-        long long a;
-        cin >> a;
-        
-        if (posicoes.find(a) != posicoes.end()) {
-            cout << posicoes[a] << '\n';
-        } else {
-            cout << -1 << '\n';
-        }
-    }
-
-    return 0;
->>>>>>> 098fa3dc8fe68a7eca34032b89144fce012441e9
 }
